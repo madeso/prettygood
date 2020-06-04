e3912e89222203701f5797091a7c4c8b49d89982
Gustav <sir.gustav.the.coder@gmail.com>
2020-06-04 08:53:37 +0200

    added test scripts

---
+++ logging-talk.py
@@ -0,0 +1,38 @@
+# from: https://www.youtube.com/watch?v=gR73nLbbgqY
+
+
+import logging
+
+logging.basicConfig(level=logging.WARNING, format="%(msg)s")
+
+if options.verbose:
+    logging.getLogger().setLevel(logging.DEBUG)
+
+LOG = logging.getLogger('logtest')
+
+LOG.debug("wakka wakka")
+
+##############################################################################
+
+# pip install colorama
+
+from colorama import Fore, Back, Style
+colorama.init(strip=True)
+print(Fore.RED + "red")
+print(Back.GREEN + "back")
+print(Style.BRIGHT + "bright")
+print(Fore.RESET + Back.RESET + Style.RESET_ALL)
+
+
+##############################################################################
+
+# pip install progressbar2
+
+from progressbar import *
+import time
+
+progress = ProgressBar()
+for i in progress(range(80)):
+    time.sleep(0.01)
+
+
